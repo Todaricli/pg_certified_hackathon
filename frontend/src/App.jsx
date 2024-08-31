3ec318f442ca0d81732aaec9152c09f77641fee8
tonexie <tone.xie@gmail.com>
2024-08-31 16:53:29 +1200

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+// import './global.css';
+import '@mantine/carousel/styles.css';
+
 import Home from './pages/home';
 import Login from './pages/Login';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <MantineProvider defaultColorScheme="dark">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </MantineProvider>
   );
 }
 
-export default App;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Button, Container, Progress, Slider } from '@mantine/core';
+
 
 const Home = () => {
   return (
-    <div>home</div>
+    <Container size={'xl'}>
+      <div>home</div>
+      <Progress value={50} my={10}/>
+
+      <Button
+        fullWidth
+      />
+      <Slider
+        color="blue"
+        marks={[
+          { value: 20, label: '20%' },
+          { value: 50, label: '50%' },
+          { value: 80, label: '80%' },
+        ]}
+      />
+    
+    </Container>
   )
 }
 
