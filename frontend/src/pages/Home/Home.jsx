f55a830c7eabcfdd3fe7f79ae938220336f87efb
jinwookuk <jkuk801@aucklanduni.ac.nz>
2024-08-31 23:20:11 +1200

    activity modal

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
-import { Progress, Text } from '@mantine/core';
+import React, { useState } from 'react';
+import { Progress, Text, Modal, Button, Select } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconChevronDown, IconChevronUp } from '@tabler/icons-react';
+import { mantine_rem as rem } from 'foxact/rem';
+import { BsLightningCharge } from "react-icons/bs";
 
 const userData = {
   "user_id": "user123",
@@ -32,6 +36,7 @@ const userData = {
   }
 };
 
+
 const Home = () => {
   // Calculate values for each stat
   const calculateHealth = (sleepHours, stressLevel) => {
@@ -63,10 +68,54 @@ const Home = () => {
   const dexterity = calculateDexterity(userData.health_data.steps.count, userData.health_data.steps.goal);
   const stamina = calculateStamina(userData.health_data.heart_rate.resting);
   const happiness = calculateHappiness(userData.health_data.streak.running_days);
+  const [opened, {open, close}] = useDisclosure(false);
+  const [dropdownOpened, setDropdownOpened] = useState(false);
 
   return (
     <div className='h-screen w-screen'>
       <div className="max-w-xl mx-4 p-4 bg-white rounded-lg shadow-md">
+        <Modal 
+        opened={opened} onClose={close} withCloseButton={false} size={"auto"} overlayProps={{backgroundOpactiy: 0.55, blur: 3,}} centered yOffset={0} xOffset={0}
+        styles={{
+          content: {
+            width: "100%",
+            height: "400px",
+            borderRadius: "20px",
+            backgroundColor: "#ffd53d",
+            maxWidth: "28rem",
+            padding: "1rem",
+          },
+        }}
+        >
+          <div className="text-lg font-bold text-center mb-2">Please choose an exercise</div>
+          <Select 
+          placeholder="Select an exercise" 
+          styles={{
+            dropdown: {
+              borderRadius: "10px",
+              padding: "10px",
+              '&:hover': {
+            backgroundColor: '#0000a3',
+            '&[data-selected]': {
+              backgroundColor: '#0000a3',
+            }
+          },
+            },
+            input: {
+              height: "45px",
+              borderWidth: "2px",
+              borderColor: "#b6b9bf",
+              color: "000000"
+            }
+          }}
+          leftSection={<BsLightningCharge style={{width: rem(16), height: rem(16),  }}/>} aria-label="Select an exercise" radius="md" data={["Walking", "Running", "Weight Training", "Swimming", "Badminton"]}  rightSection={dropdownOpened ? <IconChevronUp style={{ width: rem(16), height: rem(16) }} /> : <IconChevronDown style={{ width: rem(16), height: rem(16) }} />}
+          onDropdownOpen={() => setDropdownOpened(true)}
+          onDropdownClose={() => setDropdownOpened(false)}
+        />
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: "219px"}}>
+        <Button onClick={close} variant="gradient" gradient={{from: "#0000a3", to: "#0000a3", deg: 90}} radius="lg" >Start</Button></div>
+        </Modal>
+        <Button onClick={open}>Play</Button>
         <div className="text-xl font-bold text-center mb-4">Home</div>
         <div className="flex flex-col items-center space-y-4">
           <div className="w-full">
