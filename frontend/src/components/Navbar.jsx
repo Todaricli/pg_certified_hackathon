c0835383a9f1ac4dde5ee0d421bb0705e684bdef
jinwookuk <jkuk801@aucklanduni.ac.nz>
2024-09-01 13:45:06 +1200

    map not working

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import { Box, Avatar, Text, Flex } from "@mantine/core";
 import { Link } from "react-router-dom"; 
 import GoogleTranslate from '../components/GoogleTranslate';
+import { FaRegMap } from "react-icons/fa6";
+import { mantine_rem as rem } from 'foxact/rem';
+
+
 
 
 const Navbar = () => {
   return (
     <Flex justify="space-between" align="center" p={10}>
     <GoogleTranslate />
+    <Link to="/map">
+        <FaRegMap style={{ cursor: "pointer", width: rem(21), height: rem(21)}} />
+      </Link>
       <Link to="/user">
         <Avatar src="avatar.png" style={{ cursor: "pointer" }} />
       </Link>
--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NearbyRecommendations from '../components/NearbyRecommendations';
-import PetsMap from '../components/PetsMap';
+import PetsMap from '../components/GoogleMap';
 
 
 const Map = () => {
